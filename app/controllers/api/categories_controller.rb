bb0c3ef2028ef04be49114392a7b5bc626b19d7a
VoTrung96 <votiensy240196@gmail.com>
2018-10-10 22:16:14 +0700

    api store

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,17 +1,13 @@
 class Api::CategoriesController < ApplicationController
   before_action :load_category, only: :show
+  before_action :load_products_serializer_by_category, only: :show
 
   def index
-    categories = parse_json Category.all
-    json_response categories
+    json_response(parse_json Category.all)
   end
 
   def show
-    products_by_category = @category.products.paginate page: params[:page] ||= 1,
-      per_page: params[:per_page] ||= 10
-    products_serializer = json_pagination products_by_category, params[:page], params[:per_page],
-      products_by_category.total_pages, products_by_category.total_entries
-    hash_source = {category: @category, products: products_serializer}
+    hash_source = {category: @category, products: @products_serializer}
     json_response hash_source
   end
 
@@ -19,5 +15,12 @@ class Api::CategoriesController < ApplicationController
 
   def load_category
     @category = Category.find_by! id: params[:id]
+  end
+
+  def load_products_serializer_by_category
+    products_by_category = @category.products.paginate page: params[:page] ||= 1,
+      per_page: params[:per_page] ||= 10
+    @products_serializer = json_pagination products_by_category, params[:page], params[:per_page],
+      products_by_category.total_pages, products_by_category.total_entries
   end
 end
--- app/controllers/api/stores_controller.rb
+++ app/controllers/api/stores_controller.rb
@@ -1,8 +1,10 @@
 class Api::StoresController < ApplicationController
   before_action :load_store, only: :show
+  before_action :load_foods_serializer_by_store, :load_drinks_serializer_by_store, only: :show
 
   def index
-    stores_accepted = Store.accepted.paginate page: params[:page] ||= 1, per_page: params[:per_page] ||= 10
+    stores_accepted = Store.accepted.paginate page: params[:page] ||= 1,
+      per_page: params[:per_page] ||= 10
     stores_serializer = parse_json stores_accepted
     json_response_pagination stores_serializer, params[:page], params[:per_page],
       stores_accepted.total_pages, stores_accepted.total_entries
@@ -10,16 +12,32 @@ class Api::StoresController < ApplicationController
 
   def show
     store_serializer = parse_json @store
-    json_response store_serializer
+    products_serializer = {food: @foods_serializer, drink: @drinks_serializer}
+    hash_source = {store: store_serializer[:store], products: products_serializer}
+    json_response hash_source
   end
 
   private
 
   def load_store
-    @store = Store.find_by! id: params[:id]
+    @store = Store.find params[:id]
   end
 
   def store_params
     params.permit :name, :email, :phone, :address, :open_at, :close_at, :description, :user_id
+  end
+
+  def load_foods_serializer_by_store
+    foods_by_store = @store.products.food.paginate page: params[:page] ||= 1,
+      per_page: params[:per_page] ||= 10
+    @foods_serializer = json_pagination foods_by_store, params[:page], params[:per_page],
+      foods_by_store.total_pages, foods_by_store.total_entries
+  end
+
+  def load_drinks_serializer_by_store
+    drinks_by_store = @store.products.food.paginate page: params[:page] ||= 1,
+      per_page: params[:per_page] ||= 10
+    @drinks_serializer = json_pagination drinks_by_store, params[:page], params[:per_page],
+      drinks_by_store.total_pages, drinks_by_store.total_entries
   end
 end
