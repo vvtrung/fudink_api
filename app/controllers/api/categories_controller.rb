b5f6e7b2043649459876464e8337e0997c273255
VoTrung96 <votiensy240196@gmail.com>
2018-10-24 21:42:16 +0700

    add response get all

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,17 +1,21 @@
 class Api::CategoriesController < ApplicationController
   before_action :load_category, only: :show
-  before_action :load_products_serializer_by_category, only: :show
+  before_action :load_products_by_category, only: :show
 
   def index
     json_response(parse_json Category.all)
   end
 
   def show
-    hash_source = {
-      category: @category,
-      products: @products_serializer[:products],
-      pagination: @products_serializer[:pagination]
-    }
+    hash_source = {category: @category}
+    if params[:get_all].blank?
+      @products_serializer = json_pagination parse_json(@products), params[:page] ||= 1,
+        params[:per_page], @products.total_pages, @products.total_entries
+      hash_source[:products] = @products_serializer[:products]
+      hash_source[:pagination] = @products_serializer[:pagination]
+    else
+      hash_source[:products] = parse_json(@products)
+    end
     json_response hash_source
   end
 
@@ -21,10 +25,12 @@ class Api::CategoriesController < ApplicationController
     @category = Category.find_by! id: params[:id]
   end
 
-  def load_products_serializer_by_category
-    products_by_category = @category.products.includes(:images, :rates, :sizes, :store, :category)
-      .paginate page: params[:page] ||= 1, per_page: params[:per_page] ||= 10
-    @products_serializer = json_pagination parse_json(products_by_category), params[:page] ||= 1,
-      params[:per_page], products_by_category.total_pages, products_by_category.total_entries
+  def load_products_by_category
+    @products = if params[:get_all].blank?
+      @category.products.includes(:images, :rates, :sizes, :store, :category)
+        .paginate page: params[:page] ||= 1, per_page: params[:per_page] ||= 10
+    else
+      @category.products.includes(:images, :rates, :sizes, :store, :category)
+    end
   end
 end
